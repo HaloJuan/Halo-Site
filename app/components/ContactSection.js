82d8c65fe53f1ed702f526226b590175a78245b3
Paolo <paolo@halometrics.co>
2025-09-22 15:33:12 +1000

    Changed Contact Section v5

--- app/components/ContactSection.js
+++ app/components/ContactSection.js
@@ -130,8 +130,8 @@ export default function ContactSection() {
           </div>*/}
 
           {/* Contact Info */}
-          <div className="space-y-8 text-center">
-            <div>
+          <div className="space-y-8">
+            <div className="text-center">
               <h3 className="text-2xl font-bold text-gray-800 mb-6">Get in touch</h3>
               <p className="text-gray-600 leading-relaxed max-w-2xl mx-auto">
                 We're here to help you make the most of your business data. 
